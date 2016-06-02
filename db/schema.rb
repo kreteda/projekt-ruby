107253eb8dfbd7e808290b8a78f41530a7a44c24
Rafał Hrycikowski <susxe@daintly.com>
2016-06-02 23:53:07 +0200

    up2
    s

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130613152218) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookmarks", force: true do |t|
     t.string   "title"
     t.string   "url"
@@ -30,7 +33,7 @@ ActiveRecord::Schema.define(version: 20130613152218) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
